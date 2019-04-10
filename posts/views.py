d257dec823150d89571895c217ae34364b039de4
Jiwon Yoon <white.jcme@gmail.com>
2019-04-10 08:29:57 +0000

    Enable update and delete functions

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
+from .models import Post
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 def create(request):
@@ -8,7 +10,7 @@ def create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('posts:create')
+            return redirect('posts:list')
     else: # GET 방식
         # 포스팅을 작성하는 form을 보여줌.
         form = PostForm()
@@ -16,8 +18,30 @@ def create(request):
             'form': form
         })
 
+def list(request):
+    # 모든 포스트를 보여준다
+    posts = Post.objects.all()
+    return render(request, 'posts/list.html', {
+        'posts': posts,
+    })
+    
+#@require_POST   # POST method만 accept하고 싶을 때 => if request.method == "POST": 를 쓰는 것과 동일
+def delete(request, id):
+    post = Post.objects.get(pk=id)
+    post.delete()
+    return redirect('posts:list')
 
-    
-    
+def update(request, id):
+    post = Post.objects.get(pk=id)
+    if request.method == "GET":
+        form = PostForm(instance=post)
+        return render(request, 'posts/update.html', {
+            'form': form,
+        })
+    else: # "POST"
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:list')
     
     
